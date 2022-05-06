95617c6e9e8f77c7a61a2f354924de3e9acdff32
faridzam <skinel123@gmail.com>
2022-05-06 13:16:05 +0700

    2022-05-06 update 2

--- framework/app/Http/Livewire/OmsetHarian.php
+++ framework/app/Http/Livewire/OmsetHarian.php
@@ -126,6 +126,103 @@ class OmsetHarian extends Component
 
         }
 
+    }
+
+    public function render(){
+
+        $this->stores = pos_store_desktop::where('id_store', '<', 1000)->get();
+
+        if ($this->types == "0") {
+
+            $this->omset = pos_activity_item_and_desktop::select('menu_store', 'isDell', 'created_at', 'total')
+            ->whereNotIn('isDell', [1])
+            ->whereNotIn('menu_store', [20])
+            ->whereDate('created_at', Carbon::today())
+            ->sum('total');
+
+            $this->storeRevenue = pos_activity_item_and_desktop::groupBy('menu_store')
+            ->whereNotIn('isDell', [1])
+            ->whereNotIn('menu_store', [20])
+            ->whereDate('created_at', Carbon::today())
+            ->selectRaw('sum(total) as total_revenue, menu_store')
+            ->orderBy('total_revenue', 'desc')
+            ->get('total_revenue', 'menu_store');
+
+
+        } elseif ($this->types == "1") {
+
+            $dateS = Carbon::now();
+            $dateE = Carbon::now()->subDays(7);
+            $this->omset = pos_activity_item_and_desktop::select('menu_store', 'isDell', 'created_at', 'total')
+            ->whereNotIn('isDell', [1])
+            ->whereNotIn('menu_store', [20])
+            ->whereBetween('created_at', [$dateS, $dateE])
+            ->sum('total');
+
+            $this->storeRevenue = pos_activity_item_and_desktop::groupBy('menu_store')
+            ->whereNotIn('isDell', [1])
+            ->whereNotIn('menu_store', [20])
+            ->whereBetween('created_at', [$dateS, $dateE])
+            ->selectRaw('sum(total) as total_revenue, menu_store')
+            ->orderBy('total_revenue', 'desc')
+            ->get('total_revenue', 'menu_store');
+
+        } elseif ($this->types == "2") {
+
+            $dateS = Carbon::now();
+            $dateE = Carbon::now()->subMonth(1);
+            $this->omset = pos_activity_item_and_desktop::select('menu_store', 'isDell', 'created_at', 'total')
+            ->whereNotIn('isDell', [1])
+            ->whereNotIn('menu_store', [20])
+            ->whereBetween('created_at', [$dateS, $dateE])
+            ->sum('total');
+
+            $this->storeRevenue = pos_activity_item_and_desktop::groupBy('menu_store')
+            ->whereNotIn('isDell', [1])
+            ->whereNotIn('menu_store', [20])
+            ->whereBetween('created_at', [$dateS, $dateE])
+            ->selectRaw('sum(total) as total_revenue, menu_store')
+            ->orderBy('total_revenue', 'desc')
+            ->get('total_revenue', 'menu_store');
+
+        } elseif ($this->types == "3") {
+
+            $this->omset = pos_activity_item_and_desktop::select('menu_store', 'isDell', 'created_at', 'total')
+            ->whereNotIn('isDell', [1])
+            ->whereNotIn('menu_store', [20])
+            ->whereYear('created_at', Carbon::now()->year)
+            ->sum('total');
+
+            $this->storeRevenue = pos_activity_item_and_desktop::groupBy('menu_store')
+            ->whereNotIn('isDell', [1])
+            ->whereNotIn('menu_store', [20])
+            ->whereYear('created_at', Carbon::now()->year)
+            ->selectRaw('sum(total) as total_revenue, menu_store')
+            ->orderBy('total_revenue', 'desc')
+            ->get('total_revenue', 'menu_store');
+
+        } elseif ($this->types == "4") {
+
+            $this->omset = pos_activity_item_and_desktop::select('menu_store', 'isDell', 'created_at', 'total')
+            ->whereNotIn('isDell', [1])
+            ->whereNotIn('menu_store', [20])
+            ->sum('total');
+
+            $this->storeRevenue = pos_activity_item_and_desktop::groupBy('menu_store')
+            ->whereNotIn('isDell', [1])
+            ->whereNotIn('menu_store', [20])
+            ->selectRaw('sum(total) as total_revenue, menu_store')
+            ->orderBy('total_revenue', 'desc')
+            ->get('total_revenue', 'menu_store');
+
+        }
+
+        return view('livewire.omset-harian', [
+            'omset' => $this->omset,
+            'storeRevenue' => $this->storeRevenue,
+            'stores' => $this->stores
+        ]);
 
     }
+
 }
